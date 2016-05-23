4db36e895807cf93d0139f3a0437ba3af40b3911
JIAO Jie <thomasjiao@vip.qq.com>
2016-05-23 18:58:28 +0800

    IOUtils

--- src/main/java/me/jiaojie/ch/controller/SymbolController.java
+++ src/main/java/me/jiaojie/ch/controller/SymbolController.java
@@ -10,6 +10,7 @@
  */
 package me.jiaojie.ch.controller;
 
+import java.io.IOException;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
 
 import me.jiaojie.ch.model.basic.Symbol;
+
 /**
  *
  * @author jiaojie <jiaojie@staff.sina.com>
@@ -28,9 +33,11 @@ import me.jiaojie.ch.model.basic.Symbol;
 @Controller
 public class SymbolController {
 
+    @Autowired
+    private HttpServletRequest request;
+
 //    @Autowired
 //    private CarService carService;
-
 //    @RequestMapping("/car/list")
 //    public void carList(Model model) {
 //        List<Car> carList = carService.findAll();
@@ -52,9 +59,16 @@ public class SymbolController {
 //        carService.add(car);
 //        return "redirect:/car/list";
 //    }
-    @RequestMapping("/test")
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
     public void test() {
-        
+        InputStream is = null;
+        try {
+            is = request.getInputStream();
+            String contentStr = IOUtils.toString(is, "utf-8");
+            System.out.println(contentStr);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
-    
+
 }
