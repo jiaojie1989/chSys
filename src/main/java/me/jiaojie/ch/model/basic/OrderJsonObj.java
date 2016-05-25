875c582e7959bec4ddce944201aa163498388de9
JIAO Jie <thomasjiao@vip.qq.com>
2016-05-25 18:05:01 +0800

    接受订单的obj

---
+++ src/main/java/me/jiaojie/ch/model/basic/OrderJsonObj.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2016 SINA Corporation
+ *  
+ *  
+ * 
+ * This script is firstly created at 2016-05-25.
+ * 
+ * To see more infomation,
+ *    visit our official website http://jiaoyi.sina.com.cn/.
+ */
+package me.jiaojie.ch.model.basic;
+
+/**
+ *
+ * @author jiaojie <jiaojie@staff.sina.com>
+ */
+public class OrderJsonObj {
+
+    private String orderId;
+    private String symbol;
+    private String type;
+    private String sid;
+    private int amount;
+    private long timestamp;
+    private double price;
+
+    /**
+     * @return the orderId
+     */
+    public String getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the symbol
+     */
+    public String getSymbol() {
+        return symbol;
+    }
+
+    /**
+     * @param symbol the symbol to set
+     */
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the sid
+     */
+    public String getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+    /**
+     * @return the amount
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @param timestamp the timestamp to set
+     */
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * @return the price
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
