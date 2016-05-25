7fe38249d388504d37d3ffa4dce334e863471c5e
JIAO Jie <thomasjiao@vip.qq.com>
2016-05-25 20:59:52 +0800

    实现买卖的线程

--- src/main/java/me/jiaojie/ch/service/runner/OrderBuy.java
+++ src/main/java/me/jiaojie/ch/service/runner/OrderBuy.java
@@ -10,7 +10,15 @@
  */
 package me.jiaojie.ch.service.runner;
 
+import me.jiaojie.ch.model.basic.Order;
+import me.jiaojie.ch.model.basic.OrderDetail;
+import me.jiaojie.ch.model.basic.OrderJsonObj;
+import me.jiaojie.ch.model.basic.Price;
 import me.jiaojie.ch.model.basic.Symbol;
+import me.jiaojie.ch.model.factory.BuySellTypeFactory;
+import me.jiaojie.ch.model.factory.ProjectFactory;
+import me.jiaojie.ch.model.project.Cn;
+import me.jiaojie.ch.service.MyLogger;
 
 /**
  *
@@ -18,9 +26,24 @@ import me.jiaojie.ch.model.basic.Symbol;
  */
 public class OrderBuy implements Runnable {
 
-    protected Symbol symbol;
+    protected Order order;
+    protected String projectName;
+    protected OrderJsonObj orderObj;
+
+    public OrderBuy(OrderJsonObj orderObj, String projectName) {
+        this.orderObj = orderObj;
+        this.projectName = projectName;
+    }
 
     @Override
     public void run() {
+        Symbol symbol = Cn.getInstance().getSymbol(orderObj.getSymbol());
+        order = new Order(ProjectFactory.getProject(this.projectName), new OrderDetail(orderObj.getOrderId(), orderObj.getSid(), orderObj.getAmount(), orderObj.getTimestamp()), BuySellTypeFactory.getType("buy"), symbol, new Price(orderObj.getPrice()), this.orderObj.getTimestamp());
+        if (order.canDeal()) {
+            MyLogger.info("Succ Buy: " + order);
+        } else {
+            MyLogger.info("Queued Buy: " + order);
+            Cn.getInstance().mkBuyOrder(order);
+        }
     }
 }
